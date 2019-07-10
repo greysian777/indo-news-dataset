c14b516f2c8f676f38e462e7f5bc5074136aed92
svmihar <svmihar@gmail.com>
2019-07-10 17:05:40 +0700

    fix cron job file

--- pusher.py
+++ pusher.py
@@ -4,7 +4,7 @@ from datetime import datetime
 today = datetime.now().strftime('generated at %H:%M:%S')
 
 os.system('git add .')
-os.system(f'git commit -m {datetime.now().strftime('generated at %H:%M:%S')})
+os.system(f"git commit -m {datetime.now().strftime('generated at %H:%M:%S')}")
 os.system('git push colab master')
 
 print('manjiw!')
