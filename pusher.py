079caf68fbf5b13ebb44529e481c70628287fc96
svmihar <svmihar@gmail.com>
2019-07-10 17:02:10 +0700

    cron job files

--- pusher.py
+++ pusher.py
@@ -5,4 +5,6 @@ today = datetime.now().strftime('generated at %H:%M:%S')
 
 os.system('git add .')
 os.system(f'git commit -m {datetime.now().strftime('generated at %H:%M:%S')})
-os.system('git push origin master')
+os.system('git push colab master')
+
+print('manjiw!')
