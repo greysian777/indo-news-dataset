90bdb115a7eebf4a714444f31e591012b8937927
svmihar <svmihar@gmail.com>
2019-07-10 17:10:33 +0700

    fix job

--- pusher.py
+++ pusher.py
@@ -2,8 +2,8 @@ import os
 from datetime import datetime
 
 today = datetime.now().strftime('generated at %H:%M:%S')
-
-os.system(f"git commit -am {today}")
+os.system("git add .")
+os.system(f"git commit -m {today}")
 print(today)
 os.system('git push colab master')
 
