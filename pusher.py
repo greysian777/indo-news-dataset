5815576e89987830bf39855d61ef26f635dd6b63
svmihar <svmihar@gmail.com>
2019-07-11 07:00:39 +0000

    generated at 07:00:39 from collab

--- pusher.py
+++ pusher.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 today = datetime.now().strftime('generated at %H:%M:%S from collab')
 os.system("git add .")
-os.system(f"git commit -m \"{today}\"")
+os.system(f"git commit -m \'{today}\'")
 print(today)
 os.system('git push colab lab')
 
