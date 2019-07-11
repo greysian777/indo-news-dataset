244c2ee52c4422d3217a55af31a01c5969c8d8eb
svmihar <svmihar@gmail.com>
2019-07-11 10:21:26 +0700

    added for 2019

---
+++ kompas2019.py
@@ -0,0 +1,4 @@
+from sumber import *
+from ehe import pull_data_kompas
+
+pull_data_kompas(link, tahun2019, '2019')
--- sumber.py
+++ sumber.py
@@ -16,6 +16,15 @@ def generate_date(prev_date):
     return date_list
 
 
+def generate_date_from_today(prev_date=174):
+    from datetime import date, timedelta
+    date_list = []
+    for i in range(1, prev_date):
+        tanggal = date.today() - timedelta(i)
+        date_list.append(tanggal)
+
+    return date_list
+
 tahun_2018 = list(chunk(generate_date(366), 73))
 
 q1 = tahun_2018[0]
@@ -23,5 +32,7 @@ q2 = tahun_2018[1]
 q3 = tahun_2018[2]
 q4 = tahun_2018[3]
 q5 = tahun_2018[4]
+
+tahun2019 = generate_date_from_today()
 
 link = 'https://indeks.kompas.com/all/'
