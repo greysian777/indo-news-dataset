92f00af0723ac44027b5ce0f21a4e6e0a100d904
svmihar <svmihar@gmail.com>
2019-08-01 15:21:21 -0700

    [AUTO GENERATED] 08/01/2019, 15:21:21

--- time_bomb.py
+++ time_bomb.py
@@ -1,9 +1,9 @@
 import os 
 import time
 
-os.system('python3 pusher.py')
+os.system('python pusher.py')
 
 for _ in range(500):
     time.sleep(900)
-    os.system('python3 pusher.py')
+    os.system('python pusher.py')
     print(f'hour {_}')
