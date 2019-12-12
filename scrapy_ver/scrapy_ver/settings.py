42e1646b82a3a5eb8ec8f04670d9152af5c1ffe4
svmihar <svmihar@gmail.com>
2019-12-12 16:04:16 +0700

    403, semoga fixed, download_delay -> random

--- scrapy_ver/scrapy_ver/settings.py
+++ scrapy_ver/scrapy_ver/settings.py
@@ -8,7 +8,7 @@
 #     https://docs.scrapy.org/en/latest/topics/settings.html
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-
+import random
 BOT_NAME = 'scrapy_ver'
 
 SPIDER_MODULES = ['scrapy_ver.spiders']
@@ -24,7 +24,7 @@ CONCURRENT_REQUESTS = 10
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = random.randint(3,10)
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
